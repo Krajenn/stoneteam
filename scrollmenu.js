0f6d4f1eb13d8c29cbd07cd08b585d8a30b83384
konrad <konrad.rejentowicz@wp.pl>
2022-03-23 11:23:07 +0100

    update

--- scrollmenu.js
+++ scrollmenu.js
@@ -2,3 +2,9 @@ window.addEventListener("scroll", function () {
     var header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+$('.scrollDown').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('#sectionScrollTo').offset().top - 30
+    }, 100)
+})
