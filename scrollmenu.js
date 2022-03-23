d3d5a84661f930201a3ff316af2a97791381e9ae
konrad <konrad.rejentowicz@wp.pl>
2022-03-23 12:53:33 +0100

    updates

--- scrollmenu.js
+++ scrollmenu.js
@@ -1,10 +1,18 @@
+$('.scrollDown').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('#sectionScrollTo').offset().top - $('header').height() / 12 * 9.9
+    }, 500)
+})
+
 window.addEventListener("scroll", function () {
     var header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-})
 
-$('.scrollDown').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('#sectionScrollTo').offset().top - 20
-    }, 500)
+    let bgFixed = document.querySelector('.bgFixed');
+    let galleryPosition = document.querySelector('#sectionScrollTo').offsetTop;
+    if (window.scrollY > galleryPosition) {
+        bgFixed.classList.add('active');
+    } else if (window.scrollY < galleryPosition) {
+        bgFixed.classList.remove('active');
+    }
 })
